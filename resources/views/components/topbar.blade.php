0325146a6f54b7f4ce72ef85881eaf06a691026e
Goodie <godluckmsangi3@gmail.com>
2026-05-21 17:03:53 +0300

    settings

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -107,7 +107,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>Settings</span>
+                <span class="hidden lg:inline">Settings</span>
             </a>
 
             {{-- Logout Form - submits POST request to /logout endpoint --}}
