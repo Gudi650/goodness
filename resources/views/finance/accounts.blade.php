cb8dd2ad9b91ee0a1b16b925670f6325a2e56fcb
Goodie <godluckmsangi3@gmail.com>
2026-06-06 13:15:19 +0300

    addde the views as well here

--- resources/views/finance/accounts.blade.php
+++ resources/views/finance/accounts.blade.php
@@ -16,7 +16,7 @@
             <tbody class="divide-y divide-slate-200">
                 {{-- display the dynamic data here --}}
                 @forelse ($virtualAccounts as $account)
-                    <tr class="cursor-pointer" onclick="toggleDetails('acc{{ $account['id'] }}')">
+                    <tr class="cursor-pointer" >
                         <td class="px-4 py-2">{{ $account['bank_name'] }}</td>
                         <td class="px-4 py-2">{{ $account['account_number'] }}</td>
                         <td class="px-4 py-2">{{ $account['company_name'] }}</td>
@@ -34,6 +34,7 @@
                             <div class="inline-flex items-center gap-6">
 
                                 <button type="button"
+                                    onclick="toggleDetails('acc{{ $account['id'] }}')"
                                     onclick="event.stopPropagation(); toggleDetails('acc{{ $account['id'] }}')"
                                     class="text-slate-600 hover:text-slate-800 transition-colors" title="Show details">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -45,14 +46,17 @@
                                     </svg>
                                 </button>
 
-                                <button type="button" class="text-blue-600 hover:text-blue-800 transition-colors"
-                                    title="Edit Account">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.8" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m16.862 4.487 1.687-1.688a2.25 2.25 0 1 1 3.182 3.182L10.582 17.13a4.5 4.5 0 0 1-1.897 1.13L6 19l.74-2.685a4.5 4.5 0 0 1 1.13-1.897L16.862 4.487ZM16.862 4.487 19.5 7.125" />
-                                    </svg>
-                                </button>
+                                {{-- 
+                                    <button type="button" class="text-blue-600 hover:text-blue-800 transition-colors"
+                                        title="Edit Account">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.8" stroke="currentColor" class="w-4 h-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a2.25 2.25 0 1 1 3.182 3.182L10.582 17.13a4.5 4.5 0 0 1-1.897 1.13L6 19l.74-2.685a4.5 4.5 0 0 1 1.13-1.897L16.862 4.487ZM16.862 4.487 19.5 7.125" />
+                                        </svg>
+                                    </button>
+                                    
+                                 --}}
 
                             </div>
                         </td>
