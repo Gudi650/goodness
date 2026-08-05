856e85ff624e183a7b29498772eafeeb980d0dda
Goodie <godluckmsangi3@gmail.com>
2026-08-05 16:37:19 +0300

    added the changes needed

--- app/Services/Finance/BalanceSheet/CurrentLiabilitiesService.php
+++ app/Services/Finance/BalanceSheet/CurrentLiabilitiesService.php
@@ -66,7 +66,7 @@ class CurrentLiabilitiesService
                 ];
             });
         
-        dd($salaries);
+        //dd($salaries);
 
         //return the salaries
         return $salaries;
